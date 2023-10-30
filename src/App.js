ad08bd0779d7309885dfe7a858fcf0bc1b9ac8ac
caiosdeo <caiosdeo@hotmail.com>
2023-10-30 10:40:54 -0300

    fixing router to work with gh-pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import './style/App.css';
 import Navbar from './components/Navbar';
 import Cartao from './components/Cartao';
@@ -11,20 +11,20 @@ function App() {
   return (
     <>
       <ToastContainer transition={Slide}></ToastContainer>
-      <BrowserRouter>
+      <HashRouter>
         <div className='content'>
           <span className="brand centered">Bus Card</span>
             <Navbar />  
             <div className="App centered">
               <Routes>
-                <Route exact path='/' element={<Cartao />}/>
+                <Route path='/' element={<Cartao />}/>
                 <Route path='/recarga' element={<Recarga />}/>
                 <Route path='/config' element={<Config />}/>
               </Routes>
             </div>
           <Footer />
         </div>
-      </BrowserRouter>
+      </HashRouter>
     </>
   );
 }
